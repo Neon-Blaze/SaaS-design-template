bbcd80ff491c24b174ece505594d7422dcdf1b4a
Neon-Blaze <sonusharma94p@gmail.com>
2024-11-25 21:40:44 +0530

    Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,38 @@
+import Button from "@/components/Button";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ $7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6 max-w-2xl mx-auto">
+                    Impactfull design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8">
+                    Design tools shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
+                    <input
+                        type="email"
+                        placeholder="Enter you email"
+                        className="bg-transparent px-4 md:flex-1"
+                    />
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="whitespace-nowrap"
+                        size="sm"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }
