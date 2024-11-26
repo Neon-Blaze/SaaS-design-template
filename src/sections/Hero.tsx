7860a4eed93f2061451ca35327d928bc2c5ac42d
Neon-Blaze <sonusharma94p@gmail.com>
2024-11-26 18:16:46 +0530

    06: Hero Section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,25 @@
 import Button from "@/components/Button";
+import designExample1 from "@/assets/images/design-example-1.png";
+import designExample2 from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image src={designExample1} alt="Design example 1" />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designExample2} alt="Design example 2" />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block">
+                    <Pointer name="Byran" color="red" />
+                </div>
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨ $7.5M seed round raised
